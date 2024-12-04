bfe92d2e4f19a533459386ea6f8ddda920c9a200
arriagafati04 <arriagafatima422@gmail.com>
2024-12-04 13:04:37 -0600

    anova

---
+++ Script9.R
@@ -0,0 +1,32 @@
+#Fatima Daniela Arriaga de la Cerda
+#2070816
+#Anova
+#Experimento de cuatro parajes y sus diámetros
+#Parajes chihuaha
+#12/11/24
+
+#importar datos
+
+paraje <- read.csv("Datos_Rascon_Anova.csv", header = T)
+paraje$Paraje <- as.factor(paraje$Paraje)
+
+
+boxplot(paraje$DAP ~ paraje$Paraje)
+
+
+tapply(paraje$DAP, paraje$Paraje, mean)
+tapply(paraje$DAP, paraje$Paraje, var)
+
+
+shapiro.test(paraje$DAP)
+bartlett.test(paraje$DAP ~ paraje$Paraje)
+
+
+par.aov <- aov(paraje$DAP ~ paraje$Paraje)
+summary(par.aov)
+
+
+#Prueba de tukey
+
+TukeyHSD(par.aov)
+plot(TukeyHSD(par.aov))
